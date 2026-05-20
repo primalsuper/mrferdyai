5abe7f3016afd714f8ae9fba9e0f72c2e37e39dd
primalsuper <primalsuper@gmail.com>
2026-05-20 13:33:37 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://aimrferdy.net",
+	site: "https://ferdyai.pages.dev",
 	output: "static",
 	trailingSlash: "always",
 	integrations: [mdx(), sitemap()],
